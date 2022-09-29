9443213413a76de91656c443d06ef05a640e26a2
Miguel Angel Murcia Leon <mmurcial@emeal.nttdata.com>
2022-09-29 19:07:18 +0200

    geolocation

---
+++ src/app/services/airbnb-api.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Detail } from '../models/detail-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AirbnbApiService {
+
+  searchURL = 'https://airbnb-learning-api.herokuapp.com/search';
+  detailURL = 'https://airbnb-learning-api.herokuapp.com/listings/';
+
+  constructor(private http: HttpClient) { }
+
+  getDetailById(id : string) : Observable<Detail>{
+    return this.http.get<Detail>(this.detailURL + id)
+  }
+
+  getList() {
+    let geoLocation = {
+      "position": {
+          "lat": 41.390205,
+          "lng": 2.154007
+      }
+    }
+    window.navigator.geolocation.getCurrentPosition((position) => {
+      geoLocation.position.lat = position.coords.latitude;
+      geoLocation.position.lng = position.coords.longitude;
+    }
+    return this.http.post<Detail>(this.searchURL, geoLocation);
+
+  }
+
+}
+
+
