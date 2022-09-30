56d8eeef9a701b0d05f362a0c9d6c63dd7617d50
Jonathan <si.jonathanlozada@gmail.com>
2022-09-30 13:14:06 +0200

    detail component - intro section without form

--- src/app/detail-module/detail-component/detail-component.component.ts
+++ src/app/detail-module/detail-component/detail-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AirbnbApiService } from 'src/app/services/airbnb-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { Detail } from 'src/app/models/detail-model';
 
 @Component({
   selector: 'app-detail-component',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponentComponent implements OnInit {
 
-  constructor() { }
+  detail : Detail | undefined;
+
+  constructor(
+    private airbnApi : AirbnbApiService,
+    private route : ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.paramMap.get('id');
+    if(id!== null){
+      this.airbnApi.getDetailById(id)
+        .subscribe(data => this.detail = data)
+    }
   }
 
 }
